5e9317b7e1bf4d2cc5a31b66c5c84e24f1f61666
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-07-22 15:18:53 -0400

    fixed test bugs

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/backround.dart';
 
-import 'package:frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(navbar());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
