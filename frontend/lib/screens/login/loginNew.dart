c9993a0a76dbbb36efc500806aaffb056511e414
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-07-22 17:40:21 -0400

    login error handling

--- frontend/lib/screens/login/loginNew.dart
+++ frontend/lib/screens/login/loginNew.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/backround.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../main.dart';
+import '../../backround.dart';
 import '../../widgets/coolButtion.dart';
 import '../../widgets/coolText.dart';
 
@@ -17,14 +16,15 @@ class loginFirestore extends StatefulWidget {
 }
 
 class _loginFirestore extends State<loginFirestore> {
+  final UserName = TextEditingController();
+  final Password = TextEditingController();
+  bool _passwordVisible = true;
+
   @override
   void initState() {}
 
   @override
   Widget build(BuildContext context) {
-    final UserName = TextEditingController();
-    final Password = TextEditingController();
-
     return Scaffold(
         body: Center(
             child: Column(
@@ -40,8 +40,8 @@ class _loginFirestore extends State<loginFirestore> {
         const Spacer(
           flex: 2,
         ),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
+        SizedBox(
+            width: 300,
             child: TextField(
               controller: UserName,
               obscureText: false,
@@ -54,17 +54,28 @@ class _loginFirestore extends State<loginFirestore> {
         const Spacer(
           flex: 1,
         ),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
+        SizedBox(
+            width: 300,
             child: TextField(
-              controller: Password,
-              obscureText: true,
-              style: const TextStyle(fontSize: 20, fontFamily: 'Poppins'),
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-            )),
+                controller: Password,
+                obscureText: _passwordVisible,
+                style: const TextStyle(fontSize: 20, fontFamily: 'Poppins'),
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        color: Theme.of(context).primaryColorDark,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    )))),
         const Spacer(
           flex: 2,
         ),
@@ -78,26 +89,35 @@ class _loginFirestore extends State<loginFirestore> {
                   ),
                 );
               } else {
-                FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: UserName.text, password: Password.text)
-                    .then((value) {
-                  final firestoreInstance = FirebaseFirestore.instance;
+                try {
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: UserName.text, password: Password.text)
+                      .then((value) {
+                    final firestoreInstance = FirebaseFirestore.instance;
 
-                  firestoreInstance
-                      .collection("permissions")
-                      .doc(value.user?.email)
-                      .get()
-                      .then((data) async {
-                    await prefs.setString('deleteAll', data['perm']);
+                    firestoreInstance
+                        .collection("permissions")
+                        .doc(value.user?.email)
+                        .get()
+                        .then((data) async {
+                      await prefs.setString('deleteAll', data['perm']);
+                    });
+                    if (mounted) {
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => navbar()));
+                    }
+                  }).catchError((error) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                      "Error " + error.toString(),
+                    )));
                   });
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => navbar()));
-                }).catchError((error) {
-                  const SnackBar(
-                    content: Text("Please fill all fields"),
-                  );
-                });
+                } catch (error) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("Error " + error.toString()),
+                  ));
+                }
               }
             },
             text: "Login",
