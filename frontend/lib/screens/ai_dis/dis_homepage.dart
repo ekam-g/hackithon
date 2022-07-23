84bcadfd95f47c8dbda914b1f46acf32950d3e35
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-07-23 10:50:51 -0400

    dis homepage made and made data diplay and chnages index.html

--- frontend/lib/screens/ai_dis/dis_homepage.dart
+++ frontend/lib/screens/ai_dis/dis_homepage.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class DisHomepage extends StatefulWidget {
@@ -8,17 +10,72 @@ class DisHomepage extends StatefulWidget {
 }
 
 class _DisHomepageState extends State<DisHomepage> {
+  String email = FirebaseAuth.instance.currentUser?.email.toString() ?? "Error";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Distribution"),
+        title: const Text("Distribution AI"),
       ),
       body: Center(
         child: Column(
           children: [
-            Container(),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Expanded(
+                flex: 10,
+                child: StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection("stuff")
+                      .doc(email)
+                      .snapshots(),
+                  builder: (
+                    BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot,
+                  ) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const SizedBox(
+                          height: 250,
+                          width: 250,
+                          child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError) {
+                      return const Text('Error');
+                    }
+                    final display = snapshot.requireData;
+                    if (display["name1"]["name"] == null) {
+                      return const Text(
+                          "Please Upgrade Your Account to Offer More Services");
+                    }
+                    if (snapshot.hasData) {
+                      return SizedBox(
+                        width: 300,
+                        height: 450,
+                        child: Column(
+                          children: [
+                            const Spacer(),
+                            Text(display["name1"]["name"].toString()),
+                            const Spacer(),
+                            Text(display["name2"]["name"].toString()),
+                            const Spacer(),
+                            Text(display["name3"]["name"].toString()),
+                            const Spacer(),
+                            Text(display["name4"]["name"].toString()),
+                            const Spacer(),
+                            Text(display["name5"]["name"].toString()),
+                            const Spacer(),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
