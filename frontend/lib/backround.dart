0a4373ee1dae1b63bf5b0ecd485e9cff6569b7f8
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-07-22 21:15:56 -0400

    nav bar UI redone

--- frontend/lib/backround.dart
+++ frontend/lib/backround.dart
@@ -1,80 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:bottom_bar/bottom_bar.dart';
+import 'package:flutter/services.dart';
 import 'package:frontend/screens/HomePage.dart';
 import 'package:frontend/screens/defender/defendUI.dart';
 import 'package:frontend/screens/login/Settings.dart';
 import 'package:frontend/screens/textthingy.dart';
-import 'package:frontend/widgets/coolText.dart';
+import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
 
 class navbar extends StatefulWidget {
+  const navbar({Key? key}) : super(key: key);
+
   @override
   _navbar createState() => _navbar();
 }
 
 class _navbar extends State<navbar> {
-  int _currentPage = 0;
-  final _pageController = PageController();
+  final Color navigationBarColor = Colors.white;
+  int selectedIndex = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: selectedIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children:   [
-          const MyHomePage(title: 'Home'),
-          defender(),
-          const ML(),
-          const Settings(),
-        ],
-        onPageChanged: (index) {
-          setState(() => _currentPage = index);
-        },
+    /// [AnnotatedRegion<SystemUiOverlayStyle>] only for android black navigation bar. 3 button navigation control (legacy)
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        systemNavigationBarColor: navigationBarColor,
+        systemNavigationBarIconBrightness: Brightness.dark,
       ),
-      bottomNavigationBar: BottomBar(
-        textStyle: const TextStyle(fontWeight: FontWeight.bold),
-        selectedIndex: _currentPage,
-        onTap: (int index) {
-          _pageController.jumpToPage(index);
-          setState(() => _currentPage = index);
-        },
-        items: <BottomBarItem>[
-          BottomBarItem(
-            icon: const Icon(Icons.home),
-            title: coolText(
-              text: 'Home',
-              fontSize: 12,
+      child: Scaffold(
+        backgroundColor: Colors.grey,
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: pageController,
+          children: <Widget>[
+            const MyHomePage(title: 'Home'),
+            const Center(child: Text("implent this")),
+            defender(),
+            const ML(),
+            const Settings(),
+          ],
+        ),
+        bottomNavigationBar: WaterDropNavBar(
+          backgroundColor: navigationBarColor,
+          onItemSelected: (int index) {
+            setState(() {
+              selectedIndex = index;
+            });
+            pageController.animateToPage(selectedIndex,
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeOutQuad);
+          },
+          selectedIndex: selectedIndex,
+          barItems: <BarItem>[
+            BarItem(
+              filledIcon: Icons.home_rounded,
+              outlinedIcon: Icons.home_outlined,
             ),
-            activeColor: Colors.blue,
-            activeTitleColor: Colors.blue.shade600,
-          ),
-          BottomBarItem(
-            icon: const Icon(Icons.safety_check_rounded),
-            title: coolText(
-              text: 'Defender',
-              fontSize: 10,
+            BarItem(
+                filledIcon: Icons.favorite_rounded,
+                outlinedIcon: Icons.favorite_border_rounded,
             ),
-            activeColor: Colors.red,
-          ),
-          BottomBarItem(
-            icon: const Icon(Icons.text_fields_sharp),
-            title: coolText(
-              text: 'Text Scanner',
-              fontSize: 9,
+            BarItem(
+              filledIcon: Icons.safety_check_rounded,
+              outlinedIcon: Icons.safety_check_outlined,
             ),
-            backgroundColorOpacity: 0.1,
-            activeColor: Colors.greenAccent.shade700,
-          ),
-          BottomBarItem(
-            icon: const Icon(Icons.settings),
-            title: coolText(
-              text: 'Settings',
-              fontSize: 12,
+            BarItem(
+              filledIcon: Icons.text_increase_sharp,
+              outlinedIcon: Icons.text_fields_outlined,
             ),
-            activeColor: Colors.orange,
-            activeIconColor: Colors.orange.shade600,
-            activeTitleColor: Colors.orange.shade700,
-          ),
-        ],
+            BarItem(
+              filledIcon: Icons.settings_applications_outlined,
+              outlinedIcon: Icons.settings_applications_rounded,
+            ),
+          ],
+        ),
       ),
     );
   }
